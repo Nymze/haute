9cc936bcedebe5d5498ddf13259ac5816c31d32b
Arnaud <arnaud.meneroud@gmail.com>
2019-11-05 18:07:08 +0000

    videosphere

--- src/App.js
+++ src/App.js
@@ -9,16 +9,18 @@ function App() {
       <a-scene>
         <a-assets>
           <a-asset-item id="tube" src=""></a-asset-item>
+          <video
+            id="blue"
+            autoplay
+            loop="true"
+            src="/blue.mp4"
+            autoplay
+          ></video>
         </a-assets>
-        <a-entity
-          scale="0.1 0.1 0.1 "
-          gltf-model="url(/tube.gltf)"
-          position="0 0 0"
-          animation="property: position; to: 0 0 4000; dur: 30000; easing: linear"
-        ></a-entity>
-        <a-sky color="#ECECEC"></a-sky>
+
+        <a-videosphere src="blue.mp4"></a-videosphere>
         <a-entity id="rig" position="0 0 0">
-          <a-entity id="camera" camera></a-entity>
+          <a-camera fov="30"></a-camera>
         </a-entity>
       </a-scene>
     </div>
